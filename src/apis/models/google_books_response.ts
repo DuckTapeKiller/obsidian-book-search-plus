7d44382e4c49e53015492c9b177c110c593e9a71
DuckTapeKiller <154851905+DuckTapeKiller@users.noreply.github.com>
2026-01-09 10:41:47 +0000

    Clarify ESLint rule for GoogleBooksResponse
    Updated ESLint comment to clarify intent.

--- src/apis/models/google_books_response.ts
+++ src/apis/models/google_books_response.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any  */
+/* eslint-disable @typescript-eslint/no-explicit-any -- Allow any for API response interfaces */
 export interface GoogleBooksResponse {
   kind: string;
   totalItems: number;
